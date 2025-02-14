f4a2d163de585d2b45029668dee89a86551d10bf
ZXColt <zxcolt@gmail.com>
2025-02-14 00:23:14 -0600

    Update entry point in ecosystem configuration to reflect new file structure

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
     apps: [
         {
             name: 'dl-api',
-            script: 'index.js', // Replace with the entry point of your application
+            script: 'src/index.js', // Replace with the entry point of your application
             instances: 'max', // Or a number of instances
             exec_mode: 'cluster', // Or 'fork' for single instance
             watch: true, // Watch for file changes and restart
