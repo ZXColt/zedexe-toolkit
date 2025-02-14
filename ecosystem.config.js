97695b8a95806f60ce9283c744c39635c74d90f8
ZXColt <zxcolt@gmail.com>
2025-02-14 00:31:44 -0600

    Update ecosystem configuration to run a single instance in 'fork' mode

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,8 +3,8 @@ module.exports = {
         {
             name: 'dl-api',
             script: 'src/index.js', // Replace with the entry point of your application
-            //instances: 'max', // Or a number of instances
-            exec_mode: 'fork', // Or 'fork' for single instance
+            instances: 1, // Run a single instance
+            exec_mode: 'fork', // Use 'fork' for single instance
             watch: true, // Watch for file changes and restart
             env: {
                 NODE_ENV: 'development',
